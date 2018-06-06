32e4f9ad92d0766fcefd9be8c43ffdd604f59e21
Tech-A38 <tech-a38@Tech-A38s-MacBook-Air.local>
2018-06-06 22:51:11 +0100

    completed homework

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -1,22 +1,45 @@
 class Fibonacci
 
-  def fibonacci_array
-    array = [1 , 2]
-    i = 0
-    loop do
-      array << array[i] + array[i+1]
-      i += 1
-    break if i == 4
-    end
-    puts array
-    return array
+  attr_accessor :fibonacci_array
+
+  def initialize
+    @fibonacci_array = [1, 2]
   end
 
+  def create_fibonacci_array(num)
+    i = 0
+    loop do
+      @fibonacci_array << @fibonacci_array[i] + @fibonacci_array[i+1]
+      i += 1
+    break if i == num
+    end
+    # puts @fibonacci_array
+  end
 
+  def even
+    @even_array = @fibonacci_array.select(&:even?)
+    # puts @even_array
+    return @even_array
+  end
 
+  def sum
+    @even_array.inject(0, :+)
+  end
 
-
-
+  def less_4_mill
+    i = 0
+    loop do
+      if @fibonacci_array[i+1] > 400000
+        @fibonacci_array = @fibonacci_array[0..-2]
+      else
+        @fibonacci_array << @fibonacci_array[i] + @fibonacci_array[i+1]
+        i += 1
+        next
+      end
+    break
+    end
+    # puts @fibonacci_array
+  end
 
 
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,3 @@
-require_relative '../lib/fibonacci'
-
 RSpec.configure do |config|
   config.formatter = :documentation
 end
